02c096e8458b0a290705f59b4a6a68e78d6060c3
Megan Tegeder <megan.tegeder@gmail.com>
2024-10-11 17:48:32 -0500

    initial answers and first stretch

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,100 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+let myCollection = [];
 
+function addToCollection( collection, title, artist, yearPublished ){
+  let newAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(newAlbum);
+  return newAlbum;
+}
 
+console.log(addToCollection( myCollection, 'Volcano', 'Jungle', 2023 ));
+console.log(addToCollection( myCollection, 'ForEver', 'Jungle', 2018 ));
+console.log(addToCollection( myCollection, 'Lust for Life', 'Lana Del Rey', 2017 ));
+console.log(addToCollection( myCollection, 'Random Access Memories', 'Daft Punk', 2013 ));
+console.log(addToCollection( myCollection, 'Paradise', 'Lana Del Rey', 2012 ));
+console.log(addToCollection( myCollection, 'Rumours', 'Fleetwood Mac', 1977 ));
+console.log( 'My Collection:', myCollection );
 
+function showCollection( collection ){
+  for( let i=0; i<collection.length; i++ ){
+    let newString = collection[i].title + ' by ' + collection[i].artist + ', published in year ' + collection[i].yearPublished;
+    console.log( newString );
+  }
+} // end func
+showCollection(myCollection);
 
+function findByArtist( collection, artist ){
+  let matchingResults = [];
+  for( let i=0; i<collection.length; i++){
+    if( collection[i].artist === artist ){
+      matchingResults.push(collection[i]);
+    }
+  }
+  return matchingResults;
+} // end func
+console.log(findByArtist( myCollection, 'Jungle' ));
 
+let searchObject = { artist: 'Ray Charles', yearPublished: 1957 };
+let searchObjectExists = { artist: 'Jungle', yearPublished: 2023 };
+let emptySearchObject = {};
+let partialSearchObject = { artist: '', yearPublished: 2024 };
 
+function search( collection, searchCriteria ){
+  let matchingSearchResults = [];
 
+  // If there is no argument passed:
+  if( searchCriteria === undefined){
+    console.log('No search terms provided!');
+    return collection;
+  }
+  // If the argument is an empty object:
+  else if( Object.keys(searchCriteria).length < 1){
+    console.log('The search object is empty!!');
+    return collection;
+  }
+  // If the argument is missing artist or yearPublished keys entirely:
+  else if ( Object.keys(searchCriteria).indexOf('artist') < 0 || Object.keys(searchCriteria).indexOf('yearPublished') < 0){
+    console.log('The search object is missing artist or year data!');
+    return collection;
+  }
+  // If the argument has either artist or yearPublished as an empty string
+  else if( searchCriteria.artist < 1 || searchCriteria.yearPublished < 1){
+    console.log('empty artist or year string');
+    return collection;
+  }
+  else {
+    for( let i=0; i<collection.length; i++){
+      if( collection[i].artist === searchCriteria.artist && collection[i].yearPublished === searchCriteria.yearPublished ){
+        matchingSearchResults.push(collection[i]);
+      }
+    }
+    return matchingSearchResults;
+  }
+} // end func
 
+console.log('---Searching a result with all data, but not in myCollection:');
+console.log(search( myCollection, searchObject ));
+
+console.log('---Searching a result with all data, but is in myCollection:');
+console.log(search( myCollection, searchObjectExists ));
+
+console.log('---Searching a result with an empty object:');
+console.log(search( myCollection, emptySearchObject ));
+
+console.log('---Searching a result with partially missing terms:');
+// console.log('artist:', Object.keys(partialSearchObject).indexOf('artist'));
+// console.log('yearPublished:', Object.keys(partialSearchObject).indexOf('yearPublished'));
+console.log(search( myCollection, partialSearchObject ));
+
+console.log('---Searching a result with no searchCriteria argument:');
+console.log(search( myCollection ));
+
+console.log('--- END MY WORK');
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
